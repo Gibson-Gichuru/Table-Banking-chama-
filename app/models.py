1964a296edb28788dace52e5d4d8e30455803d10
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-08-05 16:34:14 +0300

    password security setup

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 
+from sqlalchemy.orm import backref
+
 from app import db
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Permissions:
 
@@ -13,6 +17,9 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), nullable = False)
 
+    #Tables relationship setup
+    users = db.relationship('User',backref = 'role', dynamic = 'lazy')
+
     def __str__(self):
 
         return f"<Role:{self.name}>"
@@ -21,11 +28,40 @@ class User(db.Model):
 
     __tablename__ = "users"
 
+    ###Table Columns definations
+
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique = True, index = True)
     email = db.Column(db.String(120), unique = True)
     password_hash  = db.Column(db.String(240), nullable = False)
     register_date = db.Column(db.DateTime, default = datetime.utcnow)
+    role = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    """ 
+        ________________________________________________________________________
+
+                        ###PASSWORD SECURITY IMPLEMENTATION###
+        ________________________________________________________________________
+        1.Make the password attrbute unreadable
+        2. The password setter passess the encrypted version of the user passed password to password_hash for storage
+        3. Incase a user interacts with any authentication requeried route we need to verify their given password
+
+    """
+    @property
+    def password(self):
+
+        return AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+
+        return check_password_hash(self.password_hash, password)
+
+
 
 
     def __str__(self) -> str:
