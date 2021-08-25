e5ea9783b2d584acfadcb2fddcc48a560f620ba1
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-08-25 16:37:25 +0300

    User register Interface

---
+++ app/main/form.py
@@ -0,0 +1,61 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, Regexp, EqualTo
+from wtforms import ValidationError
+
+from app.models import User
+
+
+class RegistrationForm(FlaskForm):
+
+    email = StringField("email", validators=[DataRequired(), Length(1, 64), Email()])
+
+    username = StringField(
+        "username",
+        validators=[
+            DataRequired(),
+            Length(1, 64),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_.]*$",
+                0,
+                "Usernames must have onyl \
+							letters, numbers, dots or underscore",
+            ),
+        ],
+    )
+
+    password = PasswordField(
+        "password",
+        validators=[
+            DataRequired(),
+            EqualTo("password2", message="password must match"),
+        ],
+    )
+
+    password2 = PasswordField("confirm password", validators=[DataRequired()])
+
+    phonenumber = StringField("PhoneNumber", validators = [
+
+        DataRequired(),
+        Regexp(
+            "^(\+?254|0)(7)([0-9]{8})$",
+            0,
+            "Invalid Phonenumber format"
+        )
+    ])
+
+    submit = SubmitField("Register")
+
+    # custom validators
+
+    def validate_email(self, field):
+
+        if User.query.filter_by(email=field.data).first():
+
+            raise ValidationError("Email aready registered")
+
+    def validate_username(self, field):
+
+        if User.query.filter_by(username=field.data).first():
+
+            raise ValidationError("Username aready in use")
---
+++ app/main/views.py
@@ -0,0 +1,53 @@
+from flask import render_template, flash, redirect
+from flask.helpers import url_for
+from . import main
+
+from .form import  RegistrationForm
+
+from app.models import User
+
+from app import db
+
+from app.email import send_email
+
+
+
+
+@main.route("/join", methods= ["GET", "POST"])
+def join():
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+
+        user = User()
+
+        user.email = form.email.data
+        user.username = form.username.data
+        user.password = form.password.data
+        user.phone_number = form.phonenumber.data
+
+        db.session.add(user)
+
+        db.commit()
+
+        send_email(
+            user.email,
+            "account Creation", 
+            "email", 
+            token = user.generate_confirmation_token(), 
+            user = user
+            
+            )
+
+        flash(f"Link sent to{user.email}, Confirm your email!", category="message")
+
+        return render_template('register.html', form = form)
+
+    return render_template("register.html", form  = form)
+
+
+@main.route("/confirm")
+def confirm():
+
+    pass
