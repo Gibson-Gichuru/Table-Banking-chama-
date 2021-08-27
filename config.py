5f563fc27ddee055da5a378e41e89285215085bc
Gibson-Gichuru <gichuru.developer@gmail.com>
2021-08-27 17:10:34 +0300

    SSL certificate setup and user model typo fix

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 
     
-
+    SSL_DISABLE = True
     DEBUG = True
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_COMMITON_TEARDOWN = True
